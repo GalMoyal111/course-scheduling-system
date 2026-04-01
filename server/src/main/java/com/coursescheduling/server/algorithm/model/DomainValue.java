19e15ed58a57eaff5af42f616b4efa8919efc481
Gal Moyal <gal.moyal111@gmail.com>
2026-04-01 16:43:19 +0300

    feat: add DomainValue and Variable classes; define properties for scheduling algorithm

---
+++ server/src/main/java/com/coursescheduling/server/algorithm/model/DomainValue.java
@@ -0,0 +1,10 @@
+package com.coursescheduling.server.algorithm.model;
+
+public class DomainValue {
+	private int day;
+	private int startFrame;
+	private String roomId;
+	private boolean isOccupied;
+	
+	
+}
---
+++ server/src/main/java/com/coursescheduling/server/algorithm/model/Variable.java
@@ -0,0 +1,18 @@
+package com.coursescheduling.server.algorithm.model;
+
+import com.coursescheduling.server.model.LessonType;
+
+public class Variable {
+
+	private String lessonId;
+	private String courseId;
+	private String lecturerId;
+	private int cluster;
+	private LessonType type;
+	private int duration;
+	private String splitGroupId;
+	private int index;
+	private float credits;
+	private Boolean isHardCourse;
+	
+}
